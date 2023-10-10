56fb0bd3aff5fc83b06618f34abd5eb99607b03d
juanASIR <147400061+juanASIR@users.noreply.github.com>
2023-10-10 10:14:14 +0200

    Update dhcp.sh

--- dhcp.sh
+++ dhcp.sh
@@ -6,3 +6,9 @@ sudo apt update
 
 # Actualizamos paquetes instalados
 sudo apt upgrade
+
+# Instalamos isc-server-dhcp
+sudo apt install isc-server-dhcp
+
+# Configuramos la ruta /etc/default/isc-server-dhcp
+nano /etc/default/isc-server-dhcp
